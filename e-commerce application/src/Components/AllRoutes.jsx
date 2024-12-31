67b855bacf0a0279889bb87d403abac588ef8ec4
Shalini Tiwari <130977588+shalinitiwarindia@users.noreply.github.com>
2024-12-31 17:01:34 +0530

    Create AllRoutes.jsx

---
+++ e-commerce application/src/Components/AllRoutes.jsx
@@ -0,0 +1,27 @@
+// import React from "react";
+// import { Route, Routes } from "react-router-dom";
+// import Homepage from "../Pages/Homepage";
+// import Products from "../Pages/Products";
+// import Product from "../Pages/Product";
+// import Cart from "../Pages/Cart";
+
+// const AllRoutes = () => {
+//   return (
+//     <Routes>
+//       <Route path="/" element={<Homepage />} />
+//       <Route path="/products" element={<Products />} />
+//       <Route path="/product" element={<Product />} />
+//       <Route path="/cart" element={<Cart />} />
+//     </Routes>
+//   );
+// };
+
+// export default AllRoutes;
+
+import React from "react";
+
+const AllRoutes = () => {
+  return <div>AllRoutes</div>;
+};
+
+export default AllRoutes;
